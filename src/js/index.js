1254e763512403f601223f5e4326f1523e7c0352
Ilya Yalymov <chezeee@gmail.com>
2023-02-19 15:10:15 +0300

    Initial structure added

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} weatherApiResponse
+ */
+const willItRain = weatherApiResponse => {
+	// your code
+}
+
+
+// Sample usage - do not modify
+willItRain('{"now":{"temperature":18,"humidity":"30%","rainExpected":true}}'); // true
+willItRain('{"now":{"temperature":25,"humidity":"90%","rainExpected":false}}'); // false
