56c35716281dbbd4037ef31f5cb07421413eb4d3
Ilya Yalymov <chezeee@gmail.com>
2023-02-19 15:11:13 +0300

    Problem solution added

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,40 @@
 /**
  * @param {string} weatherApiResponse
  */
-const willItRain = weatherApiResponse => {
-	// your code
-}
+const willItRain = (weatherApiResponse) => {
+  return new Promise((resolve, reject) => {
+    const response = JSON.parse(weatherApiResponse);
 
+    if (response.now.hasOwnProperty("rainExpected")) {
+      resolve(response.now.rainExpected);
+    }
 
-// Sample usage - do not modify
-willItRain('{"now":{"temperature":18,"humidity":"30%","rainExpected":true}}'); // true
-willItRain('{"now":{"temperature":25,"humidity":"90%","rainExpected":false}}'); // false
+    reject("No weather data received");
+  });
+};
+
+// JSON processing
+
+willItRain('{"now":{"temperature":18,"humidity":"30%","rainExpected":true}}')
+  .then((rainExpected) => {
+    console.log(rainExpected);
+  })
+  .catch((error) => {
+    console.error(error);
+  }); // true
+
+willItRain('{"now":{"temperature":25,"humidity":"90%","rainExpected":false}}')
+  .then((rainExpected) => {
+    console.log(rainExpected);
+  })
+  .catch((error) => {
+    console.error(error);
+  }); // false
+
+willItRain('{"now":{}}')
+  .then((rainExpected) => {
+    console.log(rainExpected);
+  })
+  .catch((error) => {
+    console.error(error);
+  }); // No weather data received
